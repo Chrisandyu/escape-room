dae904665227db0297361ecac027d4e53b498487
Christopher Zhang <82842634+Chrisandyu@users.noreply.github.com>
2024-03-22 12:16:15 -0700

    first commit

---
+++ app/page.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <div> 
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center ">
+          <div className="select-none">
+          <h1 className="text-8xl font-bold m-10 hover:drop-shadow-lg whitespace-nowrap overflow-hidden">
+              Escape Room
+            </h1>
+            <Link href = "/1">
+              <button className="btn btn-secondary btn-lg">Start</button>
+            </Link>
+          </div>
+        </div>  
+      </div>
+    </div>
+  );
+};
+
+export default page;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  daisyui: {
+    themes: ["sunset"],
+  },
+  plugins: [require("daisyui")],
+  extend: {
+    dropShadow: {
+      glow: [
+        "0 0px 20px rgba(255,255, 255, 0.35)",
+        "0 0px 65px rgba(255, 255,255, 0.2)"
+      ]
+    }
+  }
+};
